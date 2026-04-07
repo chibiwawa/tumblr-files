7d9f1a12b95d54413ca9d9e90ff25d505940923b
chibiwawa <chibiwawa@gmail.com>
2026-04-07 19:10:26 -0400

    Create masonryimprov.js

---
+++ masonryimprov.js
@@ -0,0 +1,32 @@
+    var $container = $('#posts');
+
+    if (!$container.length || $container.find('li.post').length === 0) return;
+
+    $container.masonry({
+        itemSelector: 'li.post'
+    });
+
+    var $allImages = $container.find('img');
+    var imgLoaded = 0;
+    var imgTotal = $allImages.length;
+
+    function onImageReady() {
+        imgLoaded++;
+        if (imgLoaded >= imgTotal) {
+            $container.masonry('layout');
+        }
+    }
+
+    if (imgTotal === 0) {
+        $container.masonry('layout');
+    } else {
+        $allImages.each(function() {
+            if (this.complete) {
+                onImageReady();
+            } else {
+                $(this).on('load error', function() {
+                    onImageReady();
+                });
+            }
+        });
+    }
