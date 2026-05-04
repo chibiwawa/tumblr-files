4b7b550519eb763b12924b2bc9fbe55e97c8d5b3
chibiwawa <chibiwawa@gmail.com>
2026-05-03 20:21:18 -0400

    Update wisteria.js

--- wisteria.js
+++ wisteria.js
@@ -20,3 +20,84 @@ function createSakura() {
     setTimeout(function() { if (petal.parentNode) petal.remove(); }, 18000);
 }
 setInterval(createSakura, 800);
+var $cursor = $('#custom-cursor');
+var cursorX = 0, cursorY = 0;
+var actualX = 0, actualY = 0;
+
+$(document).on('mousemove', function(e) {
+    cursorX = e.clientX;
+    cursorY = e.clientY;
+});
+
+function updateCursor() {
+    actualX += (cursorX - actualX) * 0.5;
+    actualY += (cursorY - actualY) * 0.5;
+    $cursor.css({
+        left: actualX + 'px',
+        top: actualY + 'px'
+    });
+    requestAnimationFrame(updateCursor);
+}
+requestAnimationFrame(updateCursor);
+
+$(document).on('mouseenter', 'a, button, .like_button iframe, li.post img', function() {
+    $cursor.addClass('hovered');
+});
+
+$(document).on('mouseleave', 'a, button, .like_button iframe, li.post img', function() {
+    $cursor.removeClass('hovered');
+});
+
+(function() {
+    var petals = [];
+    var maxPetals = 10;
+    var colors = ['#d396ff','#c77dff','#e0aaff','#b388ff','#ce93d8','#dbb2ff','#c9a0dc','#e1bee7','#d1c4e9','#b39ddb','#f3e5f5','#e8b4d9'];
+
+    $(document).on('mousemove', function(e) {
+        if (Math.random() > 0.5) return;
+
+        var petal = document.createElement('div');
+        var size = Math.random() * 5 + 4;
+        var color = colors[Math.floor(Math.random() * colors.length)];
+
+        petal.style.cssText =
+            'position:fixed;pointer-events:none;z-index:2147483645;' +
+            'width:' + size + 'px;height:' + (size * 1.4) + 'px;' +
+            'left:' + e.clientX + 'px;top:' + e.clientY + 'px;' +
+            'background:' + color + ';opacity:0.7;' +
+            'clip-path:polygon(50% 0%,80% 30%,70% 80%,50% 100%,30% 80%,20% 30%);';
+
+        document.body.appendChild(petal);
+        petals.push(petal);
+
+        if (petals.length > maxPetals) {
+            var old = petals.shift();
+            if (old.parentNode) old.remove();
+        }
+
+        var startX = e.clientX;
+        var startY = e.clientY;
+        var drift = Math.random() * 30 - 15;
+        var rotation = Math.random() * 360;
+        var start = performance.now();
+        var duration = 800 + Math.random() * 400;
+
+        function animate(time) {
+            var p = (time - start) / duration;
+            if (p > 1) {
+                if (petal.parentNode) petal.remove();
+                var idx = petals.indexOf(petal);
+                if (idx > -1) petals.splice(idx, 1);
+                return;
+            }
+
+            petal.style.left = (startX + drift * p) + 'px';
+            petal.style.top = (startY + 30 * p) + 'px';
+            petal.style.opacity = 0.7 * (1 - p);
+            petal.style.transform = 'rotate(' + (rotation + p * 180) + 'deg) scale(' + (1 - p * 0.5) + ')';
+
+            requestAnimationFrame(animate);
+        }
+        requestAnimationFrame(animate);
+    });
+})();
